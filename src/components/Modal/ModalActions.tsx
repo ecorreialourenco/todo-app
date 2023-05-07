eaaf88fcc33aa18c392077248875373e77792fbf
Emanuel Lourenço <dodgy6@gmail.com>
2023-05-07 18:34:13 +0100

    resolve sonarqube issues

--- src/components/Modal/ModalActions.tsx
+++ src/components/Modal/ModalActions.tsx
@@ -18,12 +18,12 @@ export const ModalActions: FC<ModalActionsProps> = ({
 }) => (
   <div className="flex justify-between px-4 py-2">
     <Button
-      label={closeTitle || "Close"}
+      label={closeTitle ?? "Close"}
       className="bg-gray-400 text-white hover:bg-gray-600 mr-2"
       onClick={onClose}
     />
     <Button
-      label={submitTitle || "Save"}
+      label={submitTitle ?? "Save"}
       className={`bg-blue-400 text-white ${
         !disabled ? "hover:bg-blue-600" : "bg-gray-300 cursor-not-allowed"
       }  ml-2`}
